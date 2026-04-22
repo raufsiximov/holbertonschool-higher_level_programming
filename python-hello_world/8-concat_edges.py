9e7dcde9bdba6ce57e802c3b39ad11b5ff1fd4fa
Rauf1 <raufsiximov64@gmail.com>
2026-04-22 09:50:40 +0400

    Refactor string concatenation in 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str) 
+text = ("Python is an interpreted, interactive, object-oriented programming "
+        "language that combines remarkable power with very clear syntax")
+result = text[39:67] + text[107:112] + text[:6]
+print(result)
