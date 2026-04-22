c6f87ce893373e0aa216420aee13b24a744952e0
Rauf1 <raufsiximov64@gmail.com>
2026-04-22 09:55:00 +0400

    Correct variable name in string concatenation
    Fix variable name from 'text' to 'str' in print statement.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-text = ("Python is an interpreted, interactive, object-oriented programming "
-        "language that combines remarkable power with very clear syntax")
-result = text[39:67] + text[107:112] + text[:6]
-print(result)
+print(str[39:67] + str[107:112] + str[:6])
