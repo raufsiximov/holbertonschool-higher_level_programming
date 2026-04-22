6790a8e86ab434ff1f427ee1e1269a21de059dc2
Rauf1 <raufsiximov64@gmail.com>
2026-04-22 10:00:44 +0400

    Fix string concatenation in 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#
-#
-#
-print(str[39:66] + str[107:113] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+
+print(str[39:66] + str[-6:])
