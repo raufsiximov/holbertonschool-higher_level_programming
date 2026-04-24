ec07c77fe85485b18da0f2ac847d116f9210d3d9
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-24 15:22:15 +0000

    Fix: optimized print calls for uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new_str = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
+            new_str += chr(ord(c) - 32)
         else:
-            print("{}".format(c), end="")
-    print("")
+            new_str += c
+    print("{}".format(new_str))
