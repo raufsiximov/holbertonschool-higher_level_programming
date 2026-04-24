e1e8d201eb6c8856ba73a27b6a3c3f2e5e2452db
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-24 15:36:00 +0000

    Fix syntax error and update logic for 9-print_last_digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def uppercase(str):
-    new_str = ""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            new_str += chr(ord(c) - 32)
-        else:
-            new_str += c
-    print("{}".format(new_str)))
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
