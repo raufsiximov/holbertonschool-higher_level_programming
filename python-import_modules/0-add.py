3d8904ae563fceef13666de72871694f45932ddf
Rauf1 <raufsiximov64@gmail.com>
2026-04-25 16:25:51 +0400

    Import add function from add_0 module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # 0-add.py faylından add funksiyasını import edirik
-    add = __import__('0-add').add
-    
+    from add_0 import add
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
