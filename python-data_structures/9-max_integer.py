e5b3c512d80c53a390744f9a25ceb6e37ff32e9f
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-28 15:40:29 +0000

    Fix W293 pycodestyle error in 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,13 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    
-    # İlk elementi ən böyük kimi götürürük
+
     result = my_list[0]
-    
-    # Siyahını dövr edərək daha böyük ədəd varmı yoxlayırıq
     for i in my_list:
         if i > result:
             result = i
-            
+
     return result
