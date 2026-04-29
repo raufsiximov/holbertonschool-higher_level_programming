643d1edf3593a903f3af756e686cc59afaea67ae
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 17:19:53 +0000

    Add 0-lookup.py: returns the list of available attributes and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Lookup moduludur.
+Obyektin atributlarının siyahısını qaytarır.
+"""
+
+
+def lookup(obj):
+    """Obyektin mövcud atribut və metodlarının siyahısını qaytarır."""
+    return dir(obj)
