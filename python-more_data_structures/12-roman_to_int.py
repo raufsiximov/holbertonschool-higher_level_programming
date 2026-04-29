474fd17ac0c493ded3e796ab00259f9702f4efe3
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:49:08 +0000

    Add 12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module to convert Roman numeral to an integer.
+"""
+
+
+def roman_to_int(roman_string):
+    """
+    Converts a Roman numeral to an integer.
+    """
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+
+    for i in range(len(roman_string)):
+        # Əgər cari rəqəm növbəti rəqəmdən kiçikdirsə, onu çıxırıq
+        if i + 1 < len(roman_string) and roman_map[roman_string[i]] < roman_map[roman_string[i+1]]:
+            total -= roman_map[roman_string[i]]
+        else:
+            total += roman_map[roman_string[i]]
+
+    return total
