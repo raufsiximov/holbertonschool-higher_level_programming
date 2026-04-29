89ded21381c9dbc909a2e7f80c4b31abd63ebb66
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 17:23:48 +0000

    Fix PEP8: remove trailing whitespace

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module provides a function that returns the list of 
+This module provides a function that returns the list of
 available attributes and methods of an object.
 """
 
