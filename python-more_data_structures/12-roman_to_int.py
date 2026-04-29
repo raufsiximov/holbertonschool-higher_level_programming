a483a185655263a4a8cb9c66a01f38cc107dcdfa
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:57:43 +0000

    Fix PEP 8: E501 line length and W293 whitespace

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -11,20 +11,20 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
     total = 0
 
     for i in range(len(roman_string)):
-        # Əgər simvol map-də yoxdursa, KeyError verməmək üçün get(..., 0) istifadə edirik
-        curr_val = roman_map.get(roman_string[i], 0)
-        
+        val = roman_map.get(roman_string[i], 0)
         if i + 1 < len(roman_string):
             next_val = roman_map.get(roman_string[i + 1], 0)
-            if curr_val < next_val:
-                total -= curr_val
+            if val < next_val:
+                total -= val
             else:
-                total += curr_val
+                total += val
         else:
-            total += curr_val
+            total += val
 
     return total
