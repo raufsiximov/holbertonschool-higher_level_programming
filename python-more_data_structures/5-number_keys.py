d8b32b674809f0ff2a73e0c66c2cdf3125ddc04e
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:32:26 +0000

    Add 5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module to return the number of keys in a dictionary.
+"""
+
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    """
+    return len(a_dictionary)
