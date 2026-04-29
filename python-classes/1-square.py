2873e7ef6b6410152f80c3a4931c755f0ccc121e
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 14:22:49 +0000

    Task 1: Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 1-square
+Defines a class Square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size):
+        """
+        Initializes the square with a size.
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
