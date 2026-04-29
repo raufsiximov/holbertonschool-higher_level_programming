a7dc43c2a7727cbff7aa9efddd4dd0f0df447144
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:21:08 +0000

    Fix: Add content to files

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """
-Module to square a matrix.
+This module contains a function that computes
+the square value of all integers of a matrix.
 """
 
 
 def square_matrix_simple(matrix=[]):
     """
     Computes the square value of all integers of a matrix.
+    Args:
+        matrix: A 2D list of integers.
+    Returns:
+        A new matrix with squared values.
     """
     return [[i**2 for i in row] for row in matrix]
