cc729eecba34b2b7cda3c9b1adacb95549faf54d
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:18:01 +0000

    Fix: Add README, shebang, and docstrings for Pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Module to square a matrix.
+"""
+
+
 def square_matrix_simple(matrix=[]):
+    """
+    Computes the square value of all integers of a matrix.
+    """
     return [[i**2 for i in row] for row in matrix]
