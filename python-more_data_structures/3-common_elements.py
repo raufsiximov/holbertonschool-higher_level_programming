3f9d2d93d6f57261920a5921e982dfcdffd3a037
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:28:13 +0000

    Add 3-common_elements.py

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module to find common elements in two sets.
+"""
+
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+    """
+    return set_1 & set_2
