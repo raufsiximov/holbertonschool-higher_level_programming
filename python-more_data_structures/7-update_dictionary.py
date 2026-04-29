243099edbb668a249744f2bd60a9b9ca6c410d74
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:37:19 +0000

    Add 7-update_dictionary.py

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module to replace or add key/value in a dictionary.
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds key/value in a dictionary.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
