d1dab19fe6e740bd6a292aace30cf83ce00d9990
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-29 05:44:29 +0000

    Add 10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module to return a key with the biggest integer value.
+"""
+
+
+def best_score(a_dictionary):
+    """
+    Returns a key with the biggest integer value.
+    """
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
