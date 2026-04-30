011095f2833f035908d3c56c8b17216a4dea762f
Rauf1 <raufsiximov64@gmail.com>
2026-04-30 19:27:05 +0400

    Create task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def get_count(self):
+        return self.count
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def __iter__(self):
+        return self
