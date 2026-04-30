db9e0558ab5a468ff94be0558440a1c8a2421497
Rauf Siximov <raufsiximov64@gmail.com>
2026-04-30 14:54:05 +0000

    Add area method to BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry klasını təkmilləşdirən modul"""
+
+
+class BaseGeometry:
+    """Həndəsə fiqurları üçün əsas klas"""
+
+    def area(selfself):
+        """Sahəni hesablamalıdır, lakin hələ reallaşdırılmayıb"""
+        raise Exception("area() is not implemented")
