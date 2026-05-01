a3211f2725b821028df3e016d654bcec7dcc05fd
Rauf Siximov <raufsiximov64@gmail.com>
2026-05-01 06:52:25 +0000

    Fix PEP8: shorten long line

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Mətni faylın sonuna əlavə edir və əlavə olunan simvol sayını qaytarır."""
+    """Faylın sonuna yazır və simvol sayını qaytarır."""
     with open(filename, mode="a", encoding="utf-8") as f:
         return f.write(text)
