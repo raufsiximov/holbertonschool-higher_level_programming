666ebabedbf5b193233648e133d724e086a00ca4
Rauf1 <raufsiximov64@gmail.com>
2026-05-01 11:29:14 +0400

    Add XML serialization and deserialization functions

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import xml.etree.ElementTree as ET
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+    
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding='utf-8', xml_declaration=True)
+
+def deserialize_from_xml(filename):
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        
+        result_dict = {}
+        for child in root:
+            result_dict[child.tag] = child.text
+            
+        return result_dict
+    except (FileNotFoundError, ET.ParseError):
+        return None
