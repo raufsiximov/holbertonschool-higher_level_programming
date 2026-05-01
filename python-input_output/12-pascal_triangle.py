4db7979e001a8b86fd306908bb43ee807d3bc07e
Rauf Siximov <raufsiximov64@gmail.com>
2026-05-01 07:14:06 +0000

    Add 12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Pascal üçbucağı modulu."""
+
+
+def pascal_triangle(n):
+    """n ölçülü Pascal üçbucağını siyahı siyahısı kimi qaytarır."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_row = triangle[-1]
+        new_row = [1]
+        for i in range(len(prev_row) - 1):
+            new_row.append(prev_row[i] + prev_row[i + 1])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
