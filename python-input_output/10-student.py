f9301a121aeda329111858124d61e63619c4a3b0
Rauf Siximov <raufsiximov64@gmail.com>
2026-05-01 07:09:35 +0000

    Add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Filtr funksiyalı Student klassı."""
+
+
+class Student:
+    """Tələbə məlumatlarını saxlayan klass."""
+
+    def __init__(self, first_name, last_name, age):
+        """İnisializasiya metodu."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Obyektin lüğət təsvirini filtr ilə qaytarır."""
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
